c0768f36461e883468019c0bf652a3aa46d4fe3b
sumonaung23 <sumonaung575@gmail.com>
2025-10-20 21:55:38 +0700

    modified the icon with ternary operator

--- src/components/FAQList.jsx
+++ src/components/FAQList.jsx
@@ -28,9 +28,9 @@ const FAQList = ({toggleDarkMode, darkMode}) => {
             <div className="flex flex-column sm:flex-row justify-between items-center mb-8 gap-4">
                 <h2 className='text-3xl font-bold bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 text-transparent bg-clip-text text-center sm:text-left w-full sm:w-auto'>Frequently Asked Questions</h2>
                 <div className="flex items-center space-x-4">
-                    <button className="flex items-center gap-2 px-4 py-2 text-sm font-medium text-white bg-gradient-to-r from-blue-500 to-indigo-500 hover:from-blue-600 hover:to-indigo-700 rounded-lg shadow-sm hover:shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 transition-all duration-300 cursor-pointer">
-                        <i className='bx bx-collapse-alt text-lg'></i>
-                        <span>Expand All</span>
+                    <button className="flex items-center gap-2 px-4 py-2 text-sm font-medium text-white bg-gradient-to-r from-blue-500 to-indigo-500 hover:from-blue-600 hover:to-indigo-700 rounded-lg shadow-sm hover:shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 transition-all duration-300 cursor-pointer" onClick={toggleExpandAll}>
+                        <i className={`bx ${expandAll ? 'bx-collapse-alt' : 'bx-expand-alt'} text-lg`}></i>
+                        <span>{expandAll ? "Collapse All" : "Expand All"}</span>
                     </button>
 
                     <button onClick={toggleDarkMode} className='w-10 h-10 flex items-center justify-center rounded-full bg-gradient-to-r from-gray-200 to-gray-300 dark:from-gray-700 dark:to-gray-800 dark:text-gray-200 text-gray-700 hover:shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 transition-all duration-300 cursor-pointer'>
